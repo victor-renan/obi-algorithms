3f1fb2d2c581c42a33c6557be2f01ba649a03978
Renan <alvesrenanpro@gmail.com>
2023-07-21 15:17:48 -0300

    feat: resolução 2021, f2, Cálculo Rápido

---
+++ 2021/f2/calculo.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Complexidade: O((B-A) * log B)
+int main()
+{
+  int S, A, B;
+
+  cin >> S >> A >> B;
+
+  if (S >= 1 && S <= 36)
+  {
+    if (A >= 1 && A <= 10000 && B >= 1 && B <= 10000 && A <= B)
+    {
+      vector<int> nums;
+
+      for (int i = A; i <= B; i++)
+      {
+        string numStr = to_string(i);
+        int sumAlgs = 0;
+
+        for (auto alg : numStr)
+        {
+          sumAlgs += int(alg - '0');
+        }
+
+        if (sumAlgs == S)
+        {
+          nums.push_back(sumAlgs);
+        }
+      }
+
+      cout << nums.size();
+    }
+  }
+}
---
+++ 2021/f2/index.md
@@ -0,0 +1,3 @@
+# Enunciates
+
+### calculo.cpp [Click Here](https://olimpiada.ic.unicamp.br/pratique/p2/2021/f2/calculo/)
